1c52f7bb534e8f7c1debc5509a0baee3b0989f61
Denys Almazov <almazovdenys@gmail.com>
2024-07-15 20:51:39 +0300

    task4: init commit

---
+++ bot_helper.py
@@ -0,0 +1,35 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+def get_number(args, contacts):
+    name = args[0]
+    return f"{name}'s phone: {contacts[name]}"
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "phone":
+            print(get_number(args, contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
